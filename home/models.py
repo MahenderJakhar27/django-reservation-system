9daadacccc038033870a9c9066b05013d3628550
Mahender Jakhar <mahenderjakhar@LP-000800s-MacBook-Pro.local>
2025-11-24 02:10:58 +0530

    new booking unique code

--- home/models.py
+++ home/models.py
@@ -11,5 +11,11 @@ class Reservation(models.Model):
     guest_count = models.IntegerField()
     reservation_date = models.DateTimeField(auto_now=True)
     commants = models.CharField(max_length=200)
+    booking_number = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    def save(self, *args, **kwargs):
+        if not self.booking_number:
+            import uuid
+            self.booking_number = str(uuid.uuid4())[:8]   # small unique code
+        super().save(*args, **kwargs)
 
      
