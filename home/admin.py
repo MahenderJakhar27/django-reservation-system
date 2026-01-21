70219378c9afa9f1229cb573ea1c85aba42da981
Mahender Jakhar <mahenderjakhar@LP-000800s-MacBook-Pro.local>
2026-01-21 15:04:36 +0530

    payment table

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
-from .models import Reservation
+from .models import SampleModel, Reservation, Payment
 
-admin.site.register(Reservation)
-# Register your models here.
+
+@admin.register(SampleModel)
+class SampleModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+
+
+class PaymentInline(admin.TabularInline):
+    model = Payment
+    extra = 0
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'guest_count',
+        'reservation_date',
+        'booking_number',
+    )
+    search_fields = ('first_name', 'last_name', 'booking_number')
+    list_filter = ('reservation_date',)
+    inlines = [PaymentInline]
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'reservation',
+        'amount',
+        'payment_date',
+    )
